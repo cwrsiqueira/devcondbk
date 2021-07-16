510918a7dd1a78b7b2474918c4b45aa86515e865
cwrsiqueira <cwrsiqueira@msn.com>
2021-07-16 00:35:53 -0300

    Até a Route Docs pronta

--- app/Http/Controllers/DocController.php
+++ app/Http/Controllers/DocController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+use App\Models\Doc;
 
 class DocController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = ['error'=>''];
+
+        $docs = Doc::all();
+        foreach ($docs as $dockey => $docvalue)
+        {
+            $docs[$dockey]['fileurl'] = asset('storage/'.$docvalue['fileurl']);
+        }
+
+        $array['list'] = $docs;
+
+        return $array;
+    }
 }
