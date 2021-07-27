fa88d071ebd8be719e39dec5fa60fb5006ac0b65
cwrsiqueira <cwrsiqueira@msn.com>
2021-07-27 01:02:47 -0300

    Em andamento

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -3,8 +3,109 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\FoundAndLost;
 
 class FoundAndLostController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = ['error' => ''];
+
+        $lost = FoundAndLost::where('status', 'LOST')
+        ->orderBy('datecreated', 'DESC')
+        ->orderBy('id', 'DESC')
+        ->get();
+
+        $recovered = FoundAndLost::where('status', 'RECOVERED')
+        ->orderBy('datecreated', 'DESC')
+        ->orderBy('id', 'DESC')
+        ->get();
+
+        foreach ($lost as $key => $value)
+        {
+            $lost[$key]['datecreated'] = date('d/m/Y', \strtotime($value['datecreated']));
+            $lost[$key]['photo'] = asset('storage/'.$value['photo']);
+        }
+
+        foreach ($recovered as $key => $value)
+        {
+            $recovered[$key]['datecreated'] = date('d/m/Y', \strtotime($value['datecreated']));
+            $recovered[$key]['photo'] = asset('storage/'.$value['photo']);
+        }
+
+        $array['lost'] = $lost;
+        $array['recovered'] = $recovered;
+
+        return $array;
+    }
+
+    public function insert(Request $request)
+    {
+        $array = ['error' => ''];
+
+        $validator = Validator::make
+        (
+            $request->all(),
+            [
+                'description' => 'required',
+                'where' => 'required',
+                'photo' => 'required|file|mimes:jpg,png',
+            ]
+        );
+
+        if(!$validator->fails())
+        {
+            $description = $request->input('description');
+            $where = $request->input('where');
+            $file = $request->file('photo')->store('public');
+            $file = \explode('public/', $file);
+            $photo = $file[1];
+
+            $newLost = new FoundAndLost();
+            $newLost->status = 'LOST';
+            $newLost->photo = $photo;
+            $newLost->description = $description;
+            $newLost->where = $where;
+            $newLost->datecreated = date('Y-m-d');
+            $newLost->save();
+        }
+        else
+        {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+
+        return $array;
+    }
+
+    public function update($id, Request $request)
+    {
+        $array = ['error' => ''];
+
+        $status = $request->input('status');
+        if($status && in_array($status, ['lost', 'recovered']))
+        {
+            $item = FoundAndLost::find($id);
+            if($item)
+            {
+                $item->status = $status;
+                $item->save();
+            }
+            else
+            {
+                $array['error'] = 'Item inexistente';
+                return $array;
+            }
+        }
+        else
+        {
+            $array['error'] = 'Status não identificado';
+            return $array;
+        }
+
+        return $array;
+    }
 }
