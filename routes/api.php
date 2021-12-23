a33f5c03b94202fe986b0ded524a2ad17d9dec81
cwrsiqueira <cwrsiqueira@msn.com>
2021-12-23 05:43:33 -0300

    atualizacao api ping pong

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\WallController;
 use App\Http\Controllers\WarningController;
 
 Route::get('/ping', function(){
-    return ['pong'=>true];
+    return ['ping'=>'pong'];
 });
 
 Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
